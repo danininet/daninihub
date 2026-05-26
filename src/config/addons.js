70f472f5233f15c8a5ba0088842c1a1089217818
Dragan Zdravkovic <127017058+danininet@users.noreply.github.com>
2026-05-26 18:07:05 +0200

    Align addons with non-sellable runtime state

--- src/config/addons.js
+++ src/config/addons.js
@@ -1,30 +1,33 @@
 const ADDONS = Object.freeze({
   SP_100: {
     id: 'SP_100',
-    label: 'Zusätzlicher Arbeitskapazität 100',
-    publicName: 'Add-on Arbeitskapazität 100',
+    label: 'Arbeitskapazität 100',
+    publicName: 'System Power 100',
     systemPower: 100,
     currency: 'EUR',
     requiresMembership: false,
-    visibility: 'public_checkout'
+    visibility: 'blocked_until_payment_stack_connected',
+    sellable: false
   },
   SP_500: {
     id: 'SP_500',
-    label: 'Zusätzlicher Arbeitskapazität 500',
-    publicName: 'Add-on Arbeitskapazität 500',
+    label: 'Arbeitskapazität 500',
+    publicName: 'System Power 500',
     systemPower: 500,
     currency: 'EUR',
     requiresMembership: false,
-    visibility: 'public_checkout'
+    visibility: 'blocked_until_payment_stack_connected',
+    sellable: false
   },
   SP_1500: {
     id: 'SP_1500',
-    label: 'Zusätzlicher Arbeitskapazität 1500',
-    publicName: 'Add-on Arbeitskapazität 1500',
+    label: 'Arbeitskapazität 1500',
+    publicName: 'System Power 1500',
     systemPower: 1500,
     currency: 'EUR',
     requiresMembership: false,
-    visibility: 'public_checkout'
+    visibility: 'blocked_until_payment_stack_connected',
+    sellable: false
   }
 });
 

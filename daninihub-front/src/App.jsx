e513f187698ab9e74c4b016e788948bffc02391f
Dragan Zdravkovic <127017058+danininet@users.noreply.github.com>
2026-07-17 20:29:26 +0200

    Replace public AI landing with Balkan-DACH transport site

--- daninihub-front/src/App.jsx
+++ daninihub-front/src/App.jsx
@@ -1,25 +1,8 @@
 import { useState } from 'react'
 import PublicLanding from './PublicLanding'
-import InternalDashboard from './InternalDashboard'
-import { content, detectInitialLanguage } from './i18n/content'
 import './App.css'
 
-const INTERNAL_MODE = false
-
-function App() {
-  const [lang, setLang] = useState(detectInitialLanguage)
-
-  if (INTERNAL_MODE) {
-    return <InternalDashboard />
-  }
-
-  return (
-    <PublicLanding
-      lang={lang}
-      setLang={setLang}
-      t={content[lang] || content.en}
-    />
-  )
+export default function App() {
+  const [lang, setLang] = useState(() => location.pathname.startsWith('/sr') ? 'sr' : 'de')
+  return <PublicLanding lang={lang} setLang={setLang} />
 }
-
-export default App

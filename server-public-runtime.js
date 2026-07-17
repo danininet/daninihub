22d6791a036e4b7010a56c73f6081597f17cce02
Dragan Zdravkovic <127017058+danininet@users.noreply.github.com>
2026-07-17 21:41:58 +0200

    Serve complete legal procedures glossary routes and sitemap

--- server-public-runtime.js
+++ server-public-runtime.js
@@ -81,7 +81,8 @@ function mountPublicRuntime(app) {
 
   const siteRoutes = [
     '/', '/de', '/de/', '/sr', '/sr/',
-    '/de/impressum', '/de/datenschutz', '/sr/impressum', '/sr/privatnost'
+    '/de/impressum', '/de/datenschutz', '/de/cookies', '/de/haftungsausschluss', '/de/praxis-wissen', '/de/glossar',
+    '/sr/impressum', '/sr/privatnost', '/sr/kolacici', '/sr/odricanje-odgovornosti', '/sr/praksa-propisi', '/sr/recnik'
   ];
   siteRoutes.forEach(route => app.get(route, (req, res) => {
     res.sendFile(path.join(front, 'index.html'));
@@ -91,7 +92,9 @@ function mountPublicRuntime(app) {
     'User-agent: *\nAllow: /\nSitemap: https://daninihub.com/sitemap.xml\n'
   ));
   app.get('/sitemap.xml', (req, res) => res.type('application/xml').send(
-    '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>https://daninihub.com/de/</loc></url><url><loc>https://daninihub.com/sr/</loc></url></urlset>'
+    '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' +
+    ['/de/','/sr/','/de/impressum','/de/datenschutz','/de/cookies','/de/haftungsausschluss','/de/praxis-wissen','/de/glossar','/sr/impressum','/sr/privatnost','/sr/kolacici','/sr/odricanje-odgovornosti','/sr/praksa-propisi','/sr/recnik'].map(route => `<url><loc>https://daninihub.com${route}</loc></url>`).join('') +
+    '</urlset>'
   ));
   app.get('/api/public-layer', (req, res) => res.json({
     ok: true,

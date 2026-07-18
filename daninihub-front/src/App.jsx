737da3e95fc73b8d8c736e8f1492d0ed7b0b57d6
Dragan Zdravkovic <127017058+danininet@users.noreply.github.com>
2026-07-18 16:44:24 +0200

    Use responsive navigation on public homepage

--- daninihub-front/src/App.jsx
+++ daninihub-front/src/App.jsx
@@ -3,6 +3,7 @@ import PublicLanding from './PublicLanding'
 import PilotCheck from './PilotCheck'
 import BusinessPages from './BusinessPages'
 import ServiceQuickNav from './ServiceQuickNav'
+import SiteNavigation from './SiteNavigation'
 import './App.css'
 
 export default function App() {
@@ -25,5 +26,6 @@ export default function App() {
   if (/pilot-check|provera-pilota/.test(location.pathname)) return <PilotCheck lang={lang}/>
   if (/leistungsrahmen|obim-usluge|continuity-support|kontinuitet-podrska|fahrerkommunikation|komunikacija-vozaci/.test(location.pathname)) return <BusinessPages lang={lang}/>
   const isHome = /^\/(de|sr)\/?$/.test(location.pathname) || location.pathname === '/'
-  return <><PublicLanding lang={lang} setLang={setLang}/>{isHome&&<ServiceQuickNav lang={lang}/>}</>
+  if (isHome) return <div className="public-app"><SiteNavigation lang={lang}/><PublicLanding lang={lang} setLang={setLang}/><ServiceQuickNav lang={lang}/></div>
+  return <PublicLanding lang={lang} setLang={setLang}/>
 }
